3152beeb6938b3ac54ac3297d286deb4095f7148
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-25 13:10:13 -0300

    lancando excecoes com filtros

---
+++ BlocoTryCatchExceptionFilters/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+    Console.WriteLine("\nInforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"\n {dividendo} / {divisor} = {resultado}");
+}
+catch (DivideByZeroException ex) when (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+{
+    Console.WriteLine("\nHoje e sexta feira e nao capturamos estas excecoes por motivos obvios... \n\nSextouuuu\n\n");
+}
+catch (Exception ex) when (ex.Message.Contains("format"))
+{
+    Console.WriteLine(ex.StackTrace);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
