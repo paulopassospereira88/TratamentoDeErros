9828260b2ef8fa0ef9e925ddf3b14914f854d3f3
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-25 12:46:06 -0300

    lancando excecoes com throw

---
+++ BlocoTryCatchThrow/Program.cs
@@ -0,0 +1,51 @@
+﻿try
+{
+    A.ProcessarA();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\nTratando o erro em Main\n");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+class A
+{
+    public static void ProcessarA()
+    {
+        try
+        {
+            B.ProcessarB();
+        }
+        catch (Exception)
+        {
+            throw;
+            Console.WriteLine("Tratando o erro em A");
+        }
+        
+    }
+}
+
+class B
+{
+    public static void ProcessarB()
+    {
+        try
+        {
+            C.ProcessarC();
+        }
+        catch (Exception)
+        {
+            throw;
+            Console.WriteLine("Tratando o erro em B");
+        }
+    }
+}
+
+class C
+{
+    public static void ProcessarC()
+    {
+        throw new NotImplementedException("Metodo nao implmentando");
+    }
+}
