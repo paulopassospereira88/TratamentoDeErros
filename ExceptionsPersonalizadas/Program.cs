b758b356191ca474680b80123b4f6bafcba8b7f1
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-12-28 19:05:48 -0300

    Tratamento personalizado de excecoes

---
+++ ExceptionsPersonalizadas/Program.cs
@@ -0,0 +1,80 @@
+﻿using ExceptionsPersonalizadas;
+
+Conta conta1 = new Conta(100, "Maria", 100.0);
+
+try
+{
+    Console.WriteLine(conta1.ToString());
+    conta1.Depositar(100);
+    Console.WriteLine($"Saldo : {conta1.Saldo}");
+    conta1.Sacar(300);
+    Console.WriteLine($"Saldo : {conta1.Saldo}");
+
+}
+catch (SaldoInsuficienteException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.HelpLink);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+class Conta
+{
+    private int numero;
+    private string? titular;
+    private double saldo;
+
+    public Conta(int numero, string? titular, double saldo)
+    {
+        this.Numero = numero;
+        this.Titular = titular;
+        this.Saldo = saldo;
+    }
+
+    public int Numero
+    {
+        get { return numero; }
+        set { numero = value; }
+    }
+
+    public string? Titular
+    {
+        get { return titular; }
+        set { titular = value; }
+    }
+
+    public double Saldo
+    {
+        get { return saldo; }
+        private set { saldo = value; }
+    }
+
+    public double Depositar(double valor)
+    {
+        saldo += valor;
+        Console.WriteLine($"Depositou : {valor}");
+        return saldo;
+    }
+
+    public double Sacar(double valor)
+    {
+        Console.WriteLine($"Sacou : {valor}");
+        if (saldo < valor)
+        {
+            throw new SaldoInsuficienteException(valor, Saldo);
+        }
+
+        saldo += valor;
+        return saldo;
+    }
+
+    public override string ToString()
+    {
+        return $"Conta: {numero}, Titular: {titular}, Saldo: {saldo}";
+    }
+
+}
---
+++ ExceptionsPersonalizadas/SaldoInsuficienteException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsPersonalizadas
+{
+    public class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(string? message) : base(message)
+        {
+        }
+
+        public SaldoInsuficienteException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public SaldoInsuficienteException(double saque, double saldo) :base($"\nException: Valor do saque {saque} e superior {saldo}")
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Nao existe saldo suficiente para o valor do saque";
+            }
+        }
+
+        public override string? HelpLink { get => "Info : https://ww.google.com"; }
+
+    }
+}
